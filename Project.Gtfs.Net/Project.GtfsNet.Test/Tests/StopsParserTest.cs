af2c176fc12a841fca250458e798d1ca9bfbdc6c
dance2die <dance2die@hotmail.com>
2015-10-20 16:56:55 -0400

    Added empty StopsParserTest

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/StopsParserTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Project.GtfsNet.Test.Tests
+{
+	public class StopsParserTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public StopsParserTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+
+	}
+}
