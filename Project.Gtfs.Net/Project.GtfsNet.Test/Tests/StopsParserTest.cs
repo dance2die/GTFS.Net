95963788777bfddb2ac8f11d5b924a953ff0daf7
dance2die <dance2die@hotmail.com>
2015-10-20 17:44:38 -0400

    Implemented "StopsParserTest.CheckAgencyDataIsParsedCorrectly"

--- Project.Gtfs.Net/Project.GtfsNet.Test/Tests/StopsParserTest.cs
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/StopsParserTest.cs
@@ -38,5 +38,31 @@ namespace Project.GtfsNet.Test.Tests
 				Assert.True(stopsList.Any());
 			}
 		}
+
+		[Fact]
+		public void CheckAgencyDataIsParsedCorrectly()
+		{
+			using (TextReader textReader = GetTextReader())
+			{
+				IEnumerable<Stops> stops = _sut.Parse(textReader);
+				List<Stops> stopsList = stops.ToList();
+
+				Stops stopsItem = stopsList[0];
+
+				Assert.Null(stopsItem.Code);
+				Assert.Null(stopsItem.Description);
+				Assert.Null(stopsItem.LocationType);
+				Assert.Null(stopsItem.ParentStation);
+				Assert.Null(stopsItem.Timezone);
+				Assert.Null(stopsItem.Url);
+				Assert.Null(stopsItem.WheelchairBoarding);
+				Assert.Null(stopsItem.Zone);
+
+				Assert.Equal("1", stopsItem.Id);
+				Assert.Equal(40.74128, stopsItem.Latitude);
+				Assert.Equal(-73.95639, stopsItem.Longitude);
+				Assert.Equal("Long Island City", stopsItem.Name);
+			}
+		}
 	}
 }
