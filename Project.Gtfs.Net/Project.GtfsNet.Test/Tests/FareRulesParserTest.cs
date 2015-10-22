5d9e1c7d34b01be7a502d6b8e86cef6967dff1c9
dance2die <dance2die@hotmail.com>
2015-10-21 20:49:07 -0400

    Implemented "fare_rules.txt" parser.

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/FareRulesParserTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.GtfsNet.Entities;
+using Project.GtfsNet.Entities.Maps;
+using Project.GtfsNet.Enums;
+using Project.GtfsNet.Parsers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Project.GtfsNet.Test.Tests
+{
+	public class FareRulesParserTest : ParserTestBase
+	{
+		private readonly EntitiesParser<FareRules, FareRulesMap> _parser = new EntitiesParser<FareRules, FareRulesMap>();
+
+		public override string TestFilePath { get; } = "feeds/subway/fare_rules.txt";
+
+		public FareRulesParserTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void FileIsNotEmpty()
+		{
+			using (TextReader textReader = GetTextReader())
+			{
+				IEnumerable<FareRules> parsed = _parser.Parse(textReader);
+				List<FareRules> parsedList = parsed.ToList();
+
+				Assert.NotNull(parsedList);
+				Assert.True(parsedList.Any());
+			}
+		}
+
+		[Fact]
+		public void CheckDataIsParsedCorrectly()
+		{
+			using (TextReader textReader = GetTextReader())
+			{
+				IEnumerable<FareRules> parsed = _parser.Parse(textReader);
+				List<FareRules> parsedList = parsed.ToList();
+
+				FareRules item = parsedList[0];
+
+				Assert.Equal("", item.ContainsId);
+				Assert.Equal("", item.DestinationId);
+				Assert.Equal("1", item.FareId);
+				Assert.Equal("", item.OriginId);
+				Assert.Equal("2315", item.RouteId);
+			}
+		}
+
+	}
+}
---
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Maps/FareRulesMap.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration;
+
+namespace Project.GtfsNet.Entities.Maps
+{
+	public class FareRulesMap : CsvClassMap<FareRules>
+	{
+		public FareRulesMap()
+		{
+			Map(e => e.FareId).Name("fare_id");
+			Map(e => e.RouteId).Name("route_id");
+			Map(e => e.OriginId).Name("origin_id");
+			Map(e => e.DestinationId).Name("destination_id");
+			Map(e => e.ContainsId).Name("contains_id");
+		}
+	}
+}
