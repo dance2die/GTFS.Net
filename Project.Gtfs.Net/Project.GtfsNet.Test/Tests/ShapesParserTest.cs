300cff9e63c53be1f640809a1a5060a6035fd451
dance2die <dontbothermewithspam@gmail.com>
2015-10-22 10:01:01 -0400

    Implemented parser for "shapes.txt"

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/ShapesParserTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Project.GtfsNet.Entities;
+using Project.GtfsNet.Entities.Maps;
+using Project.GtfsNet.Parsers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Project.GtfsNet.Test.Tests
+{
+	public class ShapesParserTestt : ParserTestBase
+	{
+		private readonly EntitiesParser<Shapes, ShapesMap> _parser = new EntitiesParser<Shapes, ShapesMap>();
+
+		public override string TestFilePath { get; } = "feeds/subway/shapes.txt";
+
+		public ShapesParserTestt(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+
+		[Fact]
+		public void FileIsNotEmpty()
+		{
+			using (TextReader textReader = GetTextReader())
+			{
+				IEnumerable<Shapes> parsed = _parser.Parse(textReader);
+				List<Shapes> parsedList = parsed.ToList();
+
+				Assert.NotNull(parsedList);
+				Assert.True(parsedList.Any());
+			}
+		}
+
+		[Fact]
+		public void CheckDataIsParsedCorrectly()
+		{
+			using (TextReader textReader = GetTextReader())
+			{
+				IEnumerable<Shapes> parsed = _parser.Parse(textReader);
+				List<Shapes> parsedList = parsed.ToList();
+
+				Shapes item = parsedList[0];
+
+				Assert.Null(item.DistanceTraveled);
+
+				Assert.Equal("0", item.Id);
+				Assert.Equal(40.7673330423, item.Latitude);
+				Assert.Equal(-73.5282357452, item.Longitude);
+				Assert.Equal(0, item.Sequence);
+			}
+		}
+	}
+}
---
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Maps/ShapesMap.cs
@@ -0,0 +1,16 @@
+﻿using CsvHelper.Configuration;
+
+namespace Project.GtfsNet.Entities.Maps
+{
+	public class ShapesMap : CsvClassMap<Shapes>
+	{
+		public ShapesMap()
+		{
+			Map(e => e.Id).Name("shape_id");
+			Map(e => e.Latitude).Name("shape_pt_lat");
+			Map(e => e.Longitude).Name("shape_pt_lon");
+			Map(e => e.Sequence).Name("shape_pt_sequence");
+			Map(e => e.DistanceTraveled).Name("shape_dist_traveled");
+		}
+	}
+}
