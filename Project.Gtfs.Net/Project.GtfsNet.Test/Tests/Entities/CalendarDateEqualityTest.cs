30fed30d2820f7c423282d18ef2d2258f958c4d1
dance2die <dance2die@hotmail.com>
2015-10-25 22:00:20 -0400

    CalendarDate implements IEqualityComparer

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/Entities/CalendarDateEqualityTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Project.GtfsNet.Entities;
+using Xunit;
+
+namespace Project.GtfsNet.Test.Tests.Entities
+{
+	public class CalendarDateEqualityTest
+	{
+		private readonly CalendarDate _sut1;
+		private readonly CalendarDate _sut2;
+
+		private const string SERVICE_ID = "1";
+		private readonly DateTime _dateValue = DateTime.Now;
+
+		public CalendarDateEqualityTest()
+		{
+			_sut1 = new CalendarDate();
+			_sut2 = new CalendarDate();
+		}
+
+		[Fact]
+		public void CheckThatTwoObjectsAreSame()
+		{
+			_sut1.ServiceId = SERVICE_ID;
+			_sut2.ServiceId = SERVICE_ID;
+
+			_sut1.Date = _dateValue;
+			_sut2.Date = _dateValue;
+
+			Assert.True(_sut1.Equals(_sut1, _sut2));
+		}
+
+		[Fact]
+		public void CheckThatTwoObjectsAreDifferent()
+		{
+			_sut1.ServiceId = SERVICE_ID;
+			_sut2.ServiceId = SERVICE_ID;
+
+			_sut1.Date = _dateValue;
+			_sut2.Date = _dateValue.Add(new TimeSpan(1));
+
+			Assert.False(_sut1.Equals(_sut1, _sut2));
+		}
+	}
+}
--- Project.Gtfs.Net/Project.GtfsNet/Entities/CalendarDate.cs
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/CalendarDate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Project.GtfsNet.Enums;
 
@@ -7,7 +8,7 @@ namespace Project.GtfsNet.Entities
 	/// <remarks>
 	/// https://github.com/OsmSharp/GTFS/blob/226a247861cf90badde49655095193ac829cf227/GTFS/Entities/CalendarDate.cs
 	/// </remarks>
-	public class CalendarDate : IEntity
+	public class CalendarDate : Entity, IEqualityComparer<CalendarDate>
 	{
 		[Required]
 		public string ServiceId { get; set; }
@@ -21,6 +22,16 @@ namespace Project.GtfsNet.Entities
 		public override string ToString()
 		{
 			return string.Format("[{0}] {1} {2}", ServiceId, Date, ExceptionType);
+		}
+
+		public bool Equals(CalendarDate x, CalendarDate y)
+		{
+			return AreEqual(x, y);
+		}
+
+		public int GetHashCode(CalendarDate obj)
+		{
+			return ComputeHashCode(obj);
 		}
 	}
 }
