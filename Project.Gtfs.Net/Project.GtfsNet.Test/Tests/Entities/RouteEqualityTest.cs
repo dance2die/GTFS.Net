b78d44bf9cf3cbbf102b11e654c4d8b41d070a7d
dance2die <dance2die@hotmail.com>
2015-10-25 22:33:35 -0400

    Route implements IEqualityComparer

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/Entities/RouteEqualityTest.cs
@@ -0,0 +1,87 @@
+﻿using Project.GtfsNet.Entities;
+using Project.GtfsNet.Enums;
+using Xunit;
+
+namespace Project.GtfsNet.Test.Tests.Entities
+{
+	public class RouteEqualityTest
+	{
+		private readonly Route _sut1;
+		private readonly Route _sut2;
+
+		private const string STRING_VALUE = "1";
+		private const RouteType ROUTE_TYPE = RouteType.AdditionalRailService;
+
+		public RouteEqualityTest()
+		{
+			_sut1 = new Route();
+			_sut2 = new Route();
+		}
+
+		[Fact]
+		public void CheckThatTwoObjectsAreSame()
+		{
+			_sut1.Id = STRING_VALUE;
+			_sut2.Id = STRING_VALUE;
+
+			_sut1.AgencyId = STRING_VALUE;
+			_sut2.AgencyId = STRING_VALUE;
+
+			_sut1.ShortName = STRING_VALUE;
+			_sut2.ShortName = STRING_VALUE;
+
+			_sut1.LongName = STRING_VALUE;
+			_sut2.LongName = STRING_VALUE;
+
+			_sut1.Description = STRING_VALUE;
+			_sut2.Description = STRING_VALUE;
+
+			_sut1.Type = ROUTE_TYPE;
+			_sut2.Type = ROUTE_TYPE;
+
+			_sut1.Url = STRING_VALUE;
+			_sut2.Url = STRING_VALUE;
+
+			_sut1.Color = STRING_VALUE;
+			_sut2.Color = STRING_VALUE;
+
+			_sut1.TextColor = STRING_VALUE;
+			_sut2.TextColor = STRING_VALUE;
+
+			Assert.True(_sut1.Equals(_sut1, _sut2));
+		}
+
+		[Fact]
+		public void CheckThatTwoObjectsAreDifferent()
+		{
+			_sut1.Id = STRING_VALUE;
+			_sut2.Id = STRING_VALUE;
+
+			_sut1.AgencyId = STRING_VALUE;
+			_sut2.AgencyId = STRING_VALUE;
+
+			_sut1.ShortName = STRING_VALUE;
+			_sut2.ShortName = STRING_VALUE;
+
+			_sut1.LongName = STRING_VALUE;
+			_sut2.LongName = STRING_VALUE;
+
+			_sut1.Description = STRING_VALUE;
+			_sut2.Description = STRING_VALUE;
+
+			_sut1.Type = ROUTE_TYPE;
+			_sut2.Type = ROUTE_TYPE;
+
+			_sut1.Url = STRING_VALUE;
+			_sut2.Url = STRING_VALUE;
+
+			_sut1.Color = STRING_VALUE;
+			_sut2.Color = STRING_VALUE;
+
+			_sut1.TextColor = STRING_VALUE;
+			_sut2.TextColor = "2";
+
+			Assert.False(_sut1.Equals(_sut1, _sut2));
+		}
+	}
+}
--- Project.Gtfs.Net/Project.GtfsNet/Entities/Route.cs
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Route.cs
@@ -1,9 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Project.GtfsNet.Enums;
 
 namespace Project.GtfsNet.Entities
 {
-	public class Route : IEntity
+	public class Route : Entity, IEqualityComparer<Route>
 	{
 		[Required]
 		public string Id { get; set; }
@@ -28,5 +29,15 @@ namespace Project.GtfsNet.Entities
 		public string Color { get; set; }
 
 		public string TextColor { get; set; }
+
+		public bool Equals(Route x, Route y)
+		{
+			return AreEqual(x, y);
+		}
+
+		public int GetHashCode(Route obj)
+		{
+			return ComputeHashCode(obj);
+		}
 	}
 }
