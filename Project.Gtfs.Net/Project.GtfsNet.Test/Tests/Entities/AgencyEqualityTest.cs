4914e0a8a2e06a4604cc6a846b24d160e45b3191
dance2die <dance2die@hotmail.com>
2015-10-25 20:30:27 -0400

    Agency implements "IEqualityComparer"

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/Entities/AgencyEqualityTest.cs
@@ -0,0 +1,87 @@
+﻿using Project.GtfsNet.Entities;
+using Xunit;
+
+namespace Project.GtfsNet.Test.Tests.Entities
+{
+	public class AgencyEqualityTest
+	{
+		private Agency _sut1;
+		private Agency _sut2;
+
+		public AgencyEqualityTest()
+		{
+			_sut1 = new Agency();
+			_sut2 = new Agency();
+		}
+
+		[Fact]
+		public void CheckThatTwoAgencyObjectsAreSame()
+		{
+			const string fareUrl = "http://www.google.com";
+			const string id = "1";
+			const string language = "en";
+			const string name = "David";
+			const string phone = "111-222-3333";
+			const string timezone = "timezone";
+			const string url = "http://www.microsoft.com";
+
+			_sut1.FareUrl = fareUrl;
+			_sut2.FareUrl = fareUrl;
+
+			_sut1.Id = id;
+			_sut2.Id = id;
+
+			_sut1.Language = language;
+			_sut2.Language = language;
+
+			_sut1.Name = name;
+			_sut2.Name = name;
+
+			_sut1.Phone = phone;
+			_sut2.Phone = phone;
+
+			_sut1.Timezone = timezone;
+			_sut2.Timezone = timezone;
+
+			_sut1.Url = url;
+			_sut2.Url = url;
+
+			Assert.True(_sut1.Equals(_sut1, _sut2));
+		}
+
+		[Fact]
+		public void CheckThatTwoAgencyObjectsAreDifferent()
+		{
+			const string fareUrl = "http://www.google.com";
+			const string id = "1";
+			const string language = "en";
+			const string name = "David";
+			const string phone = "111-222-3333";
+			const string timezone = "timezone";
+			const string url = "http://www.microsoft.com";
+
+			_sut1.FareUrl = fareUrl;
+			_sut2.FareUrl = fareUrl;
+
+			_sut1.Id = id;
+			_sut2.Id = id;
+
+			_sut1.Language = language;
+			_sut2.Language = language;
+
+			_sut1.Name = name;
+			_sut2.Name = name;
+
+			_sut1.Phone = phone;
+			_sut2.Phone = phone;
+
+			_sut1.Timezone = timezone;
+			_sut2.Timezone = timezone;
+
+			_sut1.Url = url;
+			_sut2.Url = fareUrl;	// <-- Different.
+
+			Assert.False(_sut1.Equals(_sut1, _sut2));
+		}
+	}
+}
--- Project.Gtfs.Net/Project.GtfsNet/Entities/Agency.cs
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Agency.cs
@@ -1,11 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project.GtfsNet.Entities
 {
 	/// <remarks>
 	/// https://github.com/OsmSharp/GTFS/blob/2b8f3201e65ab5dd31cdacd82b4b05d34c288204/GTFS/Entities/Agency.cs
 	/// </remarks>
-	public class Agency : IEntity
+	public class Agency : IEntity, IEqualityComparer<Agency>
 	{
 		public string Id { get; set; }
 
@@ -27,10 +28,37 @@ namespace Project.GtfsNet.Entities
 		/// <summary>
 		/// Credit <see cref="https://github.com/OsmSharp/GTFS/blob/2b8f3201e65ab5dd31cdacd82b4b05d34c288204/GTFS/Entities/Agency.cs"/>
 		/// </summary>
-		/// 
 		public override string ToString()
 		{
 			return string.Format("[{0}] {1}", Id, Name);
+		}
+
+		public override bool Equals(object obj)
+		{
+			Agency agency = obj as Agency;
+			return agency.Id == Id &&
+				   agency.Name == Name &&
+				   agency.Url == Url &&
+				   agency.Timezone == Timezone &&
+				   agency.Language == Language &&
+				   agency.Phone == Phone &&
+				   agency.FareUrl == FareUrl;
+		}
+
+		public bool Equals(Agency x, Agency y)
+		{
+			return x.Equals(y);
+		}
+
+		public int GetHashCode(Agency agency)
+		{
+			return agency.Id.GetHashCode() +
+				   agency.Name.GetHashCode() +
+				   agency.Url.GetHashCode() +
+				   agency.Timezone.GetHashCode() +
+				   agency.Language.GetHashCode() +
+				   agency.Phone.GetHashCode() +
+				   agency.FareUrl.GetHashCode();
 		}
 	}
 }
