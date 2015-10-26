100896d34c75666cf80404d00e44e1c6b679d3df
dance2die <dance2die@hotmail.com>
2015-10-25 21:24:11 -0400

    Introduced Entity class, which implements IEntity and will be a base class for all entities.

--- Project.Gtfs.Net/Project.GtfsNet/Entities/Agency.cs
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Agency.cs
@@ -1,14 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Reflection;
 
 namespace Project.GtfsNet.Entities
 {
 	/// <remarks>
 	/// https://github.com/OsmSharp/GTFS/blob/2b8f3201e65ab5dd31cdacd82b4b05d34c288204/GTFS/Entities/Agency.cs
 	/// </remarks>
-	public class Agency : IEntity, IEqualityComparer<Agency>
+	public class Agency : Entity, IEqualityComparer<Agency>
 	{
 		public string Id { get; set; }
 
@@ -35,42 +33,14 @@ namespace Project.GtfsNet.Entities
 			return string.Format("[{0}] {1}", Id, Name);
 		}
 
-		public override bool Equals(object obj)
-		{
-			Agency agency = obj as Agency;
-			if (agency == null) return false;
-
-			return AreEqual(agency, this);
-		}
-
 		public bool Equals(Agency x, Agency y)
 		{
-			return x.Equals(y);
+			return AreEqual(x, y);
 		}
 
 		public int GetHashCode(Agency agency)
 		{
 			return ComputeHashCode(agency);
-		}
-
-		protected int ComputeHashCode<T>(T obj)
-		{
-			return obj.GetType().GetProperties().Sum(property => property.GetHashCode());
-		}
-
-		protected bool AreEqual<T>(T obj1, T obj2)
-		{
-			// http://stackoverflow.com/a/2502468/4035
-			foreach (PropertyInfo property in obj1.GetType().GetProperties())
-			{
-				object value1 = property.GetValue(obj1, null);
-				object value2 = property.GetValue(obj2, null);
-
-				if (!value1.Equals(value2))
-					return false;
-			}
-
-			return true;
 		}
 	}
 }
---
+++ Project.Gtfs.Net/Project.GtfsNet/Entities/Entity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.GtfsNet.Entities
+{
+	public class Entity : IEntity
+	{
+		protected int ComputeHashCode<T>(T obj)
+		{
+			return obj.GetType().GetProperties().Sum(property => property.GetHashCode());
+		}
+
+		protected bool AreEqual<T>(T obj1, T obj2)
+		{
+			// http://stackoverflow.com/a/2502468/4035
+			foreach (PropertyInfo property in obj1.GetType().GetProperties())
+			{
+				object value1 = property.GetValue(obj1, null);
+				object value2 = property.GetValue(obj2, null);
+
+				if (!value1.Equals(value2))
+					return false;
+			}
+
+			return true;
+		}
+	}
+}
