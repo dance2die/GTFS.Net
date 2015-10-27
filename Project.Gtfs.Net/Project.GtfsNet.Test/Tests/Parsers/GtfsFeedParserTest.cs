38736936850017a5918387b29dd3320333be8cc3
dance2die <dance2die@hotmail.com>
2015-10-26 20:44:23 -0400

    created GtfsFeedParserTest

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/Parsers/GtfsFeedParserTest.cs
@@ -0,0 +1,36 @@
+﻿using Project.GtfsNet.Parsers;
+using Xunit;
+
+namespace Project.GtfsNet.Test.Tests.Parsers
+{
+	public class GtfsFeedParserTest
+	{
+		private readonly GtfsFeedParser _sut;
+		private const string FEED_PATH = "feeds/subway";
+
+		public GtfsFeedParserTest()
+		{
+			_sut = new GtfsFeedParser();
+		}
+
+		[Fact]
+		public void ParserDoesntReturnEmptyGtfsFeedProperties()
+		{
+			GtfsFeed feed = _sut.Parse(FEED_PATH);
+
+			Assert.NotNull(feed.Agencies);
+			Assert.NotNull(feed.Calendars);
+			Assert.NotNull(feed.CalendarDates);
+			Assert.NotNull(feed.FareAttributes);
+			Assert.NotNull(feed.FareRules);
+			Assert.NotNull(feed.FeedInfos);
+			Assert.NotNull(feed.Frequencies);
+			Assert.NotNull(feed.Routes);
+			Assert.NotNull(feed.Shapes);
+			Assert.NotNull(feed.Stops);
+			Assert.NotNull(feed.StopTimes);
+			Assert.NotNull(feed.Transfers);
+			Assert.NotNull(feed.Trips);
+		}
+	}
+}
