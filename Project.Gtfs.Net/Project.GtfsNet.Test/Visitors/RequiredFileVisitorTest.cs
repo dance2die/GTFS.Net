56a427def062caee0e507078010dca5952c8522d
dance2die <dance2die@hotmail.com>
2015-10-27 22:24:44 -0400

    Added "RequiredFileVisitorTest"

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Visitors/RequiredFileVisitorTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit.Abstractions;
+
+namespace Project.GtfsNet.Test.Visitors
+{
+	/// <summary>
+	/// Test if all required files are parsed.
+	/// Required file list can be found here.<see cref="https://developers.google.com/transit/gtfs/reference#feed-files"/>
+	/// </summary>
+	public class RequiredFileVisitorTest
+	{
+		private readonly ITestOutputHelper _output;
+
+		public RequiredFileVisitorTest(ITestOutputHelper output)
+		{
+			_output = output;
+		}
+	}
+}
