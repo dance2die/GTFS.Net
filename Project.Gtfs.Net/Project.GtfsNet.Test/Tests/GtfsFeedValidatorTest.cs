044b321ca22e2c93606f734deebbf11042468c08
dance2die <dance2die@hotmail.com>
2015-11-02 21:46:36 -0500

    Created "GtfsFeedValidatorTest"

---
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/GtfsFeedValidatorTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.GtfsNet.Test.Fixtures;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Project.GtfsNet.Test.Tests
+{
+	public class GtfsFeedValidatorTest : IClassFixture<VisitorPathFixture>
+	{
+		private readonly ITestOutputHelper _output;
+		private readonly VisitorPathFixture _visitorPathFixture;
+
+		public GtfsFeedValidatorTest(ITestOutputHelper output, VisitorPathFixture visitorPathFixture)
+		{
+			_output = output;
+			_visitorPathFixture = visitorPathFixture;
+		}
+	}
+}
