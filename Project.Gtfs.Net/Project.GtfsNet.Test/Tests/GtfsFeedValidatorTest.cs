ed5e8ab826c59f9b127f17cae970895025ecf7ed
dance2die <dance2die@hotmail.com>
2015-11-02 22:04:43 -0500

    Skeletal code for "GtfsFeedValidatorTest"

--- Project.Gtfs.Net/Project.GtfsNet.Test/Tests/GtfsFeedValidatorTest.cs
+++ Project.Gtfs.Net/Project.GtfsNet.Test/Tests/GtfsFeedValidatorTest.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Project.GtfsNet.Parsers;
 using Project.GtfsNet.Test.Fixtures;
+using Project.GtfsNet.Visitors;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,10 +16,34 @@ namespace Project.GtfsNet.Test.Tests
 		private readonly ITestOutputHelper _output;
 		private readonly VisitorPathFixture _visitorPathFixture;
 
+		private readonly GtfsFeed _parsedFeedGood;
+		private readonly GtfsFeed _parsedFeedNonExisting;
+
+		private readonly GtfsFeedValidator _sut;
+
 		public GtfsFeedValidatorTest(ITestOutputHelper output, VisitorPathFixture visitorPathFixture)
 		{
 			_output = output;
 			_visitorPathFixture = visitorPathFixture;
+
+			_parsedFeedGood = new GtfsFeedParser().Parse(_visitorPathFixture.GoodFeedPath);
+			_parsedFeedNonExisting = new GtfsFeedParser().Parse(_visitorPathFixture.NonExistingFeedPath);
+
+			_sut = new GtfsFeedValidator();
+		}
+
+
+	}
+
+	public class GtfsFeedValidator
+	{
+		private readonly RequiredFieldVisitor _fieldVisitor;
+		private readonly RequiredFileVisitor _fileVisitor;
+
+		public GtfsFeedValidator()
+		{
+			_fieldVisitor = new RequiredFieldVisitor();
+			_fileVisitor = new RequiredFileVisitor();
 		}
 	}
 }
