75dde8cbdf08cd48b16845e3d96a8850ce84bb32
dance2die <dance2die@hotmail.com>
2015-11-04 23:10:35 -0500

    Profiled using DotTrace: Found out that GTFS.Net is slower than OsmSharp/GTFS because of CsvReader parses slower. Well CSVReader certainly has many fucntionalities I don't need.

--- Project.Gtfs.Net/Project.GtfsNet.Demo/Program2.cs
+++ Project.Gtfs.Net/Project.GtfsNet.Demo/Program2.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using GTFS;
+using GTFS.IO;
+using Project.GtfsNet.Parsers;
 
 namespace Project.GtfsNet.Demo
 {
@@ -10,7 +10,34 @@ namespace Project.GtfsNet.Demo
 	{
 		public static void Main(string[] args)
 		{
+			TestGtfsNet();
+			TestGtfs();
+
+			Console.WriteLine("Press ENTER to continue...");
+			Console.Read();
 		}
 
+		private static void TestGtfsNet()
+		{
+			GtfsFeedParser parser = new GtfsFeedParser();
+			GtfsFeed feed = parser.Parse(Paths.GOOD_FEED_PATH);
+		}
+
+		private static void TestGtfs()
+		{
+			// create the reader.
+			var reader = new GTFSReader<GTFSFeed>(strict:false);
+
+			// execute the reader.
+			var feed = reader.Read(new GTFSDirectorySource(new DirectoryInfo(Paths.GOOD_FEED_PATH)));
+		}
 	}
+
+	public static class Paths
+	{
+		public const string GOOD_FEED_PATH = "feeds/subway";
+		public const string BAD_FEED_PATH = "feeds/missingRequiredFiles";
+		public const string NON_EXISTING_FEED_PATH = "feeds";
+	}
+
 }
