1b6de1171df3684e035baf400e0b5a79b5a672da
dance2die <dance2die@hotmail.com>
2015-11-08 14:35:46 -0500

    Removed old commented code.

--- Project.Gtfs.Net/Project.GtfsNet/GtfsFeed.cs
+++ Project.Gtfs.Net/Project.GtfsNet/GtfsFeed.cs
@@ -6,27 +6,6 @@ namespace GtfsNet
 {
 	public class GtfsFeed : IFeedElement
 	{
-		//[Required]
-		//public HashSet<Agency> Agencies { get; set; } = new HashSet<Agency>();
-		//[Required]
-		//public HashSet<Stop> Stops { get; set; } = new HashSet<Stop>();
-		//[Required]
-		//public HashSet<Route> Routes { get; set; } = new HashSet<Route>();
-		//[Required]
-		//public HashSet<Trip> Trips { get; set; } = new HashSet<Trip>();
-		//[Required]
-		//public HashSet<StopTime> StopTimes { get; set; } = new HashSet<StopTime>();
-		//[Required]
-		//public HashSet<Calendar> Calendars { get; set; } = new HashSet<Calendar>();
-
-		//public HashSet<CalendarDate> CalendarDates { get; set; } = new HashSet<CalendarDate>();
-		//public HashSet<FareAttribute> FareAttributes { get; set; } = new HashSet<FareAttribute>();
-		//public HashSet<FareRule> FareRules { get; set; } = new HashSet<FareRule>();
-		//public HashSet<Shape> Shapes { get; set; } = new HashSet<Shape>();
-		//public HashSet<Frequency> Frequencies { get; set; } = new HashSet<Frequency>();
-		//public HashSet<Transfer> Transfers { get; set; } = new HashSet<Transfer>();
-		//public HashSet<FeedInfo> FeedInfos { get; set; } = new HashSet<FeedInfo>();
-
 		[Required]
 		public AgencyCollection Agencies { get; set; } = new AgencyCollection();
 		[Required]
