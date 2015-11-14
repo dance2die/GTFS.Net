cb7c1494385db0c74bcb3eea3b7346906608b0da
Sung K. <dontbothermewithspam@gmail.com>
2015-11-14 14:09:03 -0500

    Trying out MongoDB .NET library to write to MongoDB

--- Project.Gtfs.Net/Project.GtfsNet.Demo/Program2.cs
+++ Project.Gtfs.Net/Project.GtfsNet.Demo/Program2.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using GtfsNet.Parsers;
 using GTFS;
 using GTFS.IO;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace GtfsNet.Demo
 {
@@ -19,10 +22,37 @@ namespace GtfsNet.Demo
 			Console.Read();
 		}
 
-		private static void WriteToMongoDb()
+		private static async void WriteToMongoDb()
 		{
 			var feed = GetGTfsFeed(Paths.GOOD_FEED_PATH);
 			//feed.ToJson();
+
+			var client = new MongoClient("mongodb://localhost:27017");
+			var database = client.GetDatabase("test");
+			var collection = database.GetCollection<BsonDocument>("x");
+
+			var document = new BsonDocument
+			{
+				{ "name", "MongoDB" },
+				{ "type", "Database" },
+				{ "count", 1 },
+				{ "info", new BsonDocument
+						  {
+							  { "x", 203 },
+							  { "y", 102 }
+						  }}
+			};
+
+			await collection.InsertOneAsync(document);
+
+			// generate 100 documents with a counter ranging from 0 - 99
+			var documents = Enumerable.Range(0, 100).Select(counter => new BsonDocument("counter", counter));
+
+			await collection.InsertManyAsync(documents);
+
+			var count = await collection.CountAsync(new BsonDocument());
+
+			Console.WriteLine(count);
 		}
 
 		private static GtfsFeed GetGTfsFeed(string feedPath)
@@ -33,7 +63,7 @@ namespace GtfsNet.Demo
 		private static void TestGtfs()
 		{
 			// create the reader.
-			var reader = new GTFSReader<GTFSFeed>(strict:false);
+			var reader = new GTFSReader<GTFSFeed>(strict: false);
 
 			// execute the reader.
 			var feed = reader.Read(new GTFSDirectorySource(new DirectoryInfo(Paths.GOOD_FEED_PATH)));
